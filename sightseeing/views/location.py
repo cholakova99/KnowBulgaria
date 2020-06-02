b62dffee4fe78e2dd8e1d0c49f625d9c019541d7
sabotzs <gkuklev98@gmail.com>
2020-06-02 17:44:24 +0300

    Simple detail view

--- sightseeing/views/location.py
+++ sightseeing/views/location.py
@@ -1,8 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from sightseeing.models import Location
 
 
 class LocationListView(ListView):
     model = Location
     context_object_name = "locations"
-    template_name = "list.html"
+    template_name = "locations/list.html"
+
+
+class LocationDetailView(DetailView):
+    model = Location
+    context_object_name = "location"
+    template_name = "locations/detail.html"
