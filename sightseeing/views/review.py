a1f3df05ca8063d54952a5323533e0d36fcbf825
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-06-03 19:29:42 +0300

    fix add review

--- sightseeing/views/review.py
+++ sightseeing/views/review.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from sightseeing.models import Review
+from django.shortcuts import render, get_object_or_404, redirect
+from sightseeing.models import Review, Location
 from django import forms
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def list(request):
-    return render(request, 'review/list.html', {'revieww': Review.objects.all()})
+    return render(request, 'review/list.html', {'review': Review.objects.all()})
 
 
 def detail(request, review_id):
@@ -18,16 +20,24 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ('location', 'rating', 'text', 'suggest')
 
+    # def __init__(self, user, *args, **kargs):
+    #     self.user = user
+    #     super().__init__(*args, **kargs)
 
+
+@login_required(login_url='sightseeing:login')
 def add_review(request):
+    location = Location.objects.all()
     if request.method == "POST":
         data = request.POST
         form = ReviewForm(data=data)
         if form.is_valid():
-            new_review = form.save()
+            new_review = form.save(commit=False)
+            new_review.user = request.user
+            new_review.save()
             return redirect(reverse('sightseeing:review:list'))
         else:
-            return render(request, 'review/add.html', {'form':form})
+            return render(request, 'review/add.html', {'form': form, 'location': location})
     else:
         form = ReviewForm()
-        return render(request, 'review/add.html', {'form':form})
+        return render(request, 'review/add.html', {'form': form, 'location': location})
