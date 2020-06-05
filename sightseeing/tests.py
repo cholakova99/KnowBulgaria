737e5292f8523cb1c46618a7b67e55fd7fdc7f2f
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-06-05 16:07:08 +0300

    test for login

--- sightseeing/tests.py
+++ sightseeing/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
 
-# Create your tests here.
+
+#   tests will be added
+class ReviewTestCase(TestCase):
+    @classmethod
+    def setUp(cls):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+
+        c = Client()
+        cls.logged_in = c.login(username='testuser', password='12345')
+
+    def test_logged(self):
+        self.assertTrue(self.logged_in)
